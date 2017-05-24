ba212420dbdc6b80f68966cbcf4719eda5bd2ba6
student <student@GBTC440J11LX.montgomerycollege.edu>
2017-05-24 16:31:49 -0400

    Debug activity

---
+++ src/DebugThisProgram.java
@@ -0,0 +1,24 @@
+
+public class DebugThisProgram {
+
+	public static void main(String[] args) {
+
+		/* 
+		 * Original Program
+		 * int x, y;
+		 * 
+		 * x = 3.1415; y = 3.64;
+		 * 
+		 * System.out.println("pi is approximately " + x);
+		 * System.out.println("My GPA was " + y);
+		 */
+
+		double x, y;
+
+		x = 3.1415;
+		y = 3.64;
+
+		System.out.println("pi is approximately " + x);
+		System.out.println("My GPA was " + y);
+	}
+}
